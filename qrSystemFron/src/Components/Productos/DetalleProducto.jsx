c793eb017827df37ac0686461131e5158b8ae4d1
Lucas Cein <lucascein82@gmail.com>
2023-12-07 14:32:18 -0300

    detalle prod arreglado

--- qrSystemFron/src/Components/Productos/DetalleProducto.jsx
+++ qrSystemFron/src/Components/Productos/DetalleProducto.jsx
@@ -13,38 +13,34 @@ const Detallespanroducto = () => {
             try {
                 const respuesta = await fetch(`https://qrsystemback.onrender.com/products/${productid}`);
                 const jsonData = await respuesta.json()
-                setProd(jsonData)
+                // Dividimos la cadena de fecha en sus componentes (año, mes, día)
+                let dateParts = jsonData.date.split('-');
+
+                // Convertimos los componentes en números enteros
+                // Date interpreta los meses desde 0 (enero) hasta 11 (diciembre), por lo que restamos 1 al mes
+                let year = parseInt(dateParts[0], 10);
+                let month = parseInt(dateParts[1], 10) - 1;
+                let day = parseInt(dateParts[2], 10);
+
+                // Creamos un objeto de fecha con los componentes
+                let date = new Date(year, month, day);
+
+                // Obtenemos el día, mes y año del objeto de fecha
+                // Asegurándonos de añadir un cero delante si es menor de 10
+                let formattedDay = ('0' + date.getDate()).slice(-2);
+                let formattedMonth = ('0' + (date.getMonth() + 1)).slice(-2);
+                let formattedYear = date.getFullYear();
+
+                // Construimos la cadena con el formato DD/MM/YYYY
+                 `${formattedDay}/${formattedMonth}/${formattedYear}`;
+                setProd({...jsonData,date:`${formattedDay}/${formattedMonth}/${formattedYear}`})
                 // Manejar los datos del spanroducto como sea necesario
             } catch (error) {
                 console.error('Hubo un problema con la petición Fetch:', error);
             }
         };
-
         cargarDatosProducto();
     }, [productid]);
-    console.log(prod)
-    function formatToDDMMYYYY(dateString) {
-        // Dividimos la cadena de fecha en sus componentes (año, mes, día)
-        let dateParts = dateString.split('-');
-
-        // Convertimos los componentes en números enteros
-        // Date interpreta los meses desde 0 (enero) hasta 11 (diciembre), por lo que restamos 1 al mes
-        let year = parseInt(dateParts[0], 10);
-        let month = parseInt(dateParts[1], 10) - 1;
-        let day = parseInt(dateParts[2], 10);
-
-        // Creamos un objeto de fecha con los componentes
-        let date = new Date(year, month, day);
-
-        // Obtenemos el día, mes y año del objeto de fecha
-        // Asegurándonos de añadir un cero delante si es menor de 10
-        let formattedDay = ('0' + date.getDate()).slice(-2);
-        let formattedMonth = ('0' + (date.getMonth() + 1)).slice(-2);
-        let formattedYear = date.getFullYear();
-
-        // Construimos la cadena con el formato DD/MM/YYYY
-        return `${formattedDay}/${formattedMonth}/${formattedYear}`;
-    }
     console.log(prod)
     return (
         <div className="d-flex justify-content-center align-items-center min-vh-100">
@@ -53,7 +49,7 @@ const Detallespanroducto = () => {
                 <span className="text-dark">Codigo: {prod.code}</span>
                 <div className="text-dark">Stock actual:</div>
                 <div className="ms-2 text-dark">- Bulto: {prod.quantityb} - Unidad: {prod.quantityu}</div>
-                <div className="text-dark">Fecha de Vencimiento: {formatToDDMMYYYY(prod.date)}</div>
+                <div className="text-dark">Fecha de Vencimiento: {prod.date}</div>
                 <div className="text-dark">Stock Ideal: {prod.idealstock}</div>
             </section>
         </div>
